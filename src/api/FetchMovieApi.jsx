6841c8d29ea628ebe0905d3cbef05765b4a104dc
YanLozovskyi <yanlozovskiy@gmail.com>
2023-09-16 00:34:21 +0300

    completed Api service

--- src/api/FetchMovieApi.jsx
+++ src/api/FetchMovieApi.jsx
@@ -4,14 +4,72 @@ const API_KEY = '28909b5df6d6afd9591e6fc0c7cef11e';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-const getTrendMovieByParam = async (param, controller) => {
-  const response = await axios.get(
-    `trending/movie/${param}?api_key=${API_KEY}`,
-    {
-      signal: controller.signal,
-    }
-  );
+const getTrendMoviesByParam = async (param, controller) => {
+  const response = await axios.get(`trending/movie/${param}`, {
+    params: {
+      api_key: API_KEY,
+    },
+    signal: controller.signal,
+  });
   return response.data.results;
 };
 
-export const TMDB_API = { getTrendMovieByParam };
+const getMovieDetailsById = async (movieId, controller) => {
+  const response = await axios.get(`movie/${movieId}`, {
+    params: {
+      api_key: API_KEY,
+    },
+    signal: controller.signal,
+  });
+  return response.data;
+};
+
+const getMovieCastById = async (movieId, controller) => {
+  const response = await axios.get(`movie/${movieId}/credits`, {
+    params: {
+      api_key: API_KEY,
+    },
+    signal: controller.signal,
+  });
+  return response.data.cast;
+};
+
+const getMovieReviewsById = async (movieId, controller) => {
+  const response = await axios.get(`movie/${movieId}/reviews`, {
+    params: {
+      api_key: API_KEY,
+    },
+    signal: controller.signal,
+  });
+  return response.data.results;
+};
+
+// const SearchMoviesByQuery = async (query, page, controller) => {
+//   const response = await axios.get(
+//     `search/movie?api_key=${API_KEY}&query=${query}&page=${page}`,
+//     {
+//       signal: controller.signal,
+//     }
+//   );
+//   return response.data;
+// };
+
+const SearchMoviesByQuery = async (query, page, controller) => {
+  const response = await axios.get('search/movie', {
+    params: {
+      query,
+      page,
+      api_key: API_KEY,
+    },
+    signal: controller.signal,
+  });
+  return response.data;
+};
+
+export const TMDB_API = {
+  getTrendMoviesByParam,
+  SearchMoviesByQuery,
+  getMovieDetailsById,
+  getMovieCastById,
+  getMovieReviewsById,
+};
