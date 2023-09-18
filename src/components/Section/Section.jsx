28259edbc4a00247b69daeca712e4b5ca7112a7c
YanLozovskyi <yanlozovskiy@gmail.com>
2023-09-18 15:43:20 +0300

    fix

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 export const Section = styled.section`
-  padding: 50пpx 20px;
+  padding: 50px 20px;
   background-color: ${({ theme }) => theme.colors.backgroundColorLight};
 `;
